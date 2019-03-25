0e704cc3e4b98a31dc19e39cb1c572213f72e3e0
crkylin <crkylin@gmail.com>
2019-03-25 22:10:32 +0800

    deque and queue application

---
+++ DequesAndRandomizedQueues/Permutation.java
@@ -0,0 +1,12 @@
+package DequesAndRandomizedQueues;
+
+/**
+ * @author crkylin
+ * Email:crkylin@gmail.com
+ **/
+public class Permutation {
+    public static void main(String[] args)
+    {
+
+    }
+}
