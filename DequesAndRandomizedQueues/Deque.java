20e6a817be1451959666e2c8c8ad48c1672810f9
crkylin <crkylin@gmail.com>
2019-03-28 23:33:00 +0800

    deque implements

--- DequesAndRandomizedQueues/Deque.java
+++ DequesAndRandomizedQueues/Deque.java
@@ -1,37 +1,56 @@
 package DequesAndRandomizedQueues;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * @author crkylin
  * Email:crkylin@gmail.com
  **/
 public class Deque<Item> implements Iterable<Item> {
+
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    private class Node<Item>
+    {
+        private Item element;
+        private Node front;
+        private Node next;
+
+        public Node()
+        {
+
+        }
+    }
+
     public Deque()// construct an empty deque
     {
-
+        Node head = new Node();
+        Node tail = new Node();
     }
     public boolean isEmpty()                 // is the deque empty?
     {
-
+        return head.next == null;
     }
     public int size()                        // return the number of items on the deque
     {
-
+        return this.size;
     }
     public void addFirst(Item item)          // add the item to the front
     {
 
     }
-    public void addLast(Item item)           // add the item to the end
+    public void addLast(Item item) throws IllegalArgumentException        // add the item to the end
     {
 
     }
-    public Item removeFirst()                // remove and return the item from the front
+    public Item removeFirst() throws NoSuchElementException              // remove and return the item from the front
     {
 
     }
-    public Item removeLast()                 // remove and return the item from the end
+    public Item removeLast() throws UnsupportedOperationException             // remove and return the item from the end
     {
 
     }
