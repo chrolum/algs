eb85defb19929239ec50eb3d9f730c82383e4b81
crkylin <crkylin@gmail.com>
2019-04-02 00:08:15 +0800

    randomizedQueue application

--- DequesAndRandomizedQueues/Permutation.java
+++ DequesAndRandomizedQueues/Permutation.java
@@ -1,12 +1,26 @@
 package DequesAndRandomizedQueues;
 
+import edu.princeton.cs.algs4.StdIn;
+
 /**
  * @author crkylin
  * Email:crkylin@gmail.com
  **/
 public class Permutation {
-    public static void main(String[] args)
+    public static void main(String[] args)//randomly chose k element from inout
     {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        String str = StdIn.readString();
+        while (!StdIn.isEmpty())
+        {
+            rq.enqueue(StdIn.readString());
+        }
 
+        while (k > 0)
+        {
+            System.out.println(rq.dequeue());
+            k--;
+        }
     }
 }
