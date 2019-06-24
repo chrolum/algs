eb9baa1fde92ec8b96829017bcc5d0d74522b616
crkylin <crkylin@gmail.com>
2019-06-24 18:46:48 +0800

    Design API

---
+++ Puzzle/Solver.java
@@ -0,0 +1,30 @@
+package Puzzle;
+
+/**
+ * @author crkylin
+ * Email:crkylin@gmail.com
+ **/
+public class Solver {
+    // construct a board from an n-by-n array of blocks
+    public Solver(Board initial) {
+
+    }
+    // is the initial board solvable?
+    public boolean isSolvable() {
+
+    }
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+
+    }
+
+    // solve a slider puzzle (given below)
+    public static void main(String[] args) {
+
+    }
+}
