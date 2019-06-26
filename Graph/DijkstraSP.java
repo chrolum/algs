09ccd9747efc4faa23e076734ca16cac39410600
crkylin <crkylin@gmail.com>
2019-06-27 00:24:19 +0800

    finish the function

--- Graph/DijkstraSP.java
+++ Graph/DijkstraSP.java
@@ -1,9 +1,57 @@
 package Graph;
 
+import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
+import edu.princeton.cs.algs4.IndexMinPQ;
+
 /**
  * @author crkylin
  * Email:crkylin@gmail.com
  **/
 public class DijkstraSP {
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private IndexMinPQ<Double> pq;
 
+    public DijkstraSP(EdgeWeightedDigraph G, int s) {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.E()];
+        pq = new IndexMinPQ<>(G.V());
+
+        //init all vetrix to infinity
+        for (int v = 0; v < G.V(); v++) {
+            distTo[v] = Double.POSITIVE_INFINITY;
+        }
+        distTo[s] = 0.0;
+
+        pq.insert(s, 0.0);
+        while(!pq.isEmpty()) {
+            relax(G, pq.delMin());
+        }
+    }
+
+    public double distTo(int v) {
+        return distTo[v];
+    }
+
+    public boolean hasPathTo(int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+
+    }
+
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+
+                if (pq.contains(w)) pq.change(w, distTo[w]);
+                else pq.insert(w, distTo[w]);
+            }
+        }
+    }
 }
