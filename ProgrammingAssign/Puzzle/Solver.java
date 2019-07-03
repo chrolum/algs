f7bac619097425033d511f50c78f9259d91af3ac
crkylin <crkylin@gmail.com>
2019-07-03 11:38:44 +0800

    To-do:implementation

--- ProgrammingAssign/Puzzle/Solver.java
+++ ProgrammingAssign/Puzzle/Solver.java
@@ -1,17 +1,31 @@
 package ProgrammingAssign.Puzzle;
 
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
 
 /**
  * @author crkylin
  * Email:crkylin@gmail.com
  **/
+
+/**
+ * use Manhattan priority with A* algs to solve the 8-puzzle problem
+ */
+
 public class Solver {
     private MinPQ<Board> pq;
+    private Stack<Board> solutions;
+    private int moves = -1;
 
     // construct a board from an n-by-n array of blocks
     public Solver(Board initial) {
-        
+        pq.insert(initial);
+        while (!pq.isEmpty()) {
+            Board curr = pq.delMin();
+            for (Board neighbors : curr.neighbors()) {
+                pq.insert(curr);
+            }
+        }
     }
     // is the initial board solvable?
     public boolean isSolvable() {
@@ -19,7 +33,7 @@ public class Solver {
     }
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-
+        return this.moves;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
