2fbcc85c5959517686b8a1d4abd71b4cf08b29d7
crkylin <crkylin@gmail.com>
2019-07-07 09:15:03 +0800

    Week 5 assignment

---
+++ ProgrammingAssign/kdTree/kdTree.java
@@ -0,0 +1,46 @@
+package ProgrammingAssign.kdTree;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+/**
+ * @author crkylin
+ * Email:crkylin@gmail.com
+ **/
+public class kdTree {
+    public kdTree() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void insert(Point2D p) {
+
+    }
+
+    public boolean contains(Point2D p) {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+
+    }
+
+    public Point2D nearest(Point2D p) {
+
+    }
+
+    public static void main(String... args) {
+
+    }
+}
