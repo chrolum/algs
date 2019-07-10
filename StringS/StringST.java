c6795ca991cd07da384462571102ae2d6b959626
crkylin <crkylin@gmail.com>
2019-07-11 00:11:35 +0800

    add function

--- StringS/StringST.java
+++ StringS/StringST.java
@@ -46,7 +46,23 @@ public class StringST<Value> {
     }
 
     public void delet(String key) {
+        delet(root, key, 0);
+    }
 
+    private Node delet(Node x, String key, int d) {
+        if (x == null) return null;
+        if (d == key.length()) x.val = null;
+        else {
+            char c = key.charAt(d);
+            x.next[c] = delet(x, key, d+1);
+        }
+
+        if (x.val != null) return x;
+
+        for (char c = 0; c < R; c++) {
+            if (x.next[c] != null) return x;
+        }
+        return null;
     }
 
     public boolean contains(String key) {
@@ -104,7 +120,7 @@ public class StringST<Value> {
         int d = pre.length();
         if (d == pat.length() && x.val != null) q.offer(pre);
         if (d == pat.length()) return;
-        char next = pat.charAt(d);
+        char next = pat.charAt(d);//current matching charater
         for (char c = 0; c < R; c++) {
             if (next == '.' || next == c) {
                 collect(x.next[c], pre + c, pat, q);
