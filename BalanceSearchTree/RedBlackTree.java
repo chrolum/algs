1f2c280c7d5446a3014e8681fccea21eafc3f25b
crkylin <crkylin@gmail.com>
2019-07-16 00:28:19 +0800

    BST

--- BalanceSearchTree/RedBlackTree.java
+++ BalanceSearchTree/RedBlackTree.java
@@ -4,6 +4,100 @@ package BalanceSearchTree;
  * @author crkylin
  * Email:crkylin@gmail.com
  **/
-public class RedBlackTree
-{
+public class RedBlackTree<Key extends Comparable<Key>, Value> implements SymbolTable {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root;
+
+    private class Node {
+        Key key;
+        Value val;
+        Node left, right;
+        boolean color;//color of parent link
+
+        public Node(Key key, Value val, boolean color) {
+            this.key = key;
+            this.val = val;
+            this.color = color;
+        }
+    }
+
+    public RedBlackTree() {
+
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
+    @Override
+    public void insert(Key key, Value val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node h, Key key, Value val) {
+        if (h == null) return new Node(key, val, RED);
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) h.left = put(h.left, key, val);
+        else if (cmp > 0) h.right = put(h.right, key, val);
+        else h.val = val;
+
+        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);//right-leaning case, therefore rotatelrft
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);//continuely two red-link
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);//two-red children node
+
+        return h;
+    }
+
+
+    @Override
+    public Object get(Comparable comparable) {
+        return null;
+    }
+
+    @Override
+    public void delet(Comparable comparable) {
+
+    }
+
+    @Override
+    public void deletMax() {
+
+    }
+
+    private Node deletMax(Node h) {
+
+    }
+
+    //Isometry Maintenance Operation
+    private Node rotateLeft(Node h) {
+        assert isRed(h.right);
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        assert isRed(h.left);
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
+    }
+    //temporary 4-node
+    private void flipColors(Node h) {
+        assert !isRed(h);
+        assert isRed(h.left);
+        assert isRed(h.right);
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = RED;
+    }
 }
--- BalanceSearchTree/SymbolTable.java
+++ BalanceSearchTree/SymbolTable.java
@@ -1,5 +1,26 @@
 package BalanceSearchTree;
 
-public interface SymbolTable
-{
+public interface SymbolTable<Key extends Comparable<Key>, Value> {
+    public Value get(Key key);
+
+    public void insert(Key key, Value val);
+
+    public void delet(Key key);
+
+    public void deletMax();
+
+    public void deletMin();
+
+    public Key ceiling(Key key);
+
+    public Key floor(Key key);
+
+    public Key min();
+
+    public Key max();
+
+    public int rank();//return the rank of key
+
+    public Key select();
+
 }
